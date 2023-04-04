9e66fa7065edc0bb5b1f54ed01334203977a05fb
Stanley Liu <stanley.liu@datadoghq.com>
2023-04-04 13:59:36 -0400

    Change runtime metric attribute definition

--- pkg/otlp/metrics/metrics_translator.go
+++ pkg/otlp/metrics/metrics_translator.go
@@ -36,9 +36,16 @@ import (
 // runtimeMetricMapping defines the fields needed to map OTel runtime metrics to their equivalent
 // Datadog runtime metrics
 type runtimeMetricMapping struct {
-	mappedName     string // the Datadog runtime metric name
-	attribute      string // the name of the attribute this metric originates from
-	attributeValue string // the value of the above attribute that corresponds with this metric
+	mappedName string                   // the Datadog runtime metric name
+	attributes []runtimeMetricAttribute // the attribute(s) this metric originates from
+}
+
+// runtimeMetricAttribute defines the structure for an attribute in regard to mapping runtime metrics.
+// The presence of a runtimeMetricAttribute means that a metric must be mapped from a data point
+// with the given attribute(s).
+type runtimeMetricAttribute struct {
+	key    string   // the attribute name
+	values []string // the attribute value, or multiple values if there is more than one value for the same mapping
 }
 
 var goRuntimeMetricsMappings = map[string][]runtimeMetricMapping{
@@ -60,34 +67,48 @@ var dotnetRuntimeMetricsMappings = map[string][]runtimeMetricMapping{
 	"process.runtime.dotnet.monitor.lock_contention.count": {{mappedName: "runtime.dotnet.threads.contention_count"}},
 	"process.runtime.dotnet.exceptions.count":              {{mappedName: "runtime.dotnet.exceptions.count"}},
 	"process.runtime.dotnet.gc.heap.size": {{
-		mappedName:     "runtime.dotnet.gc.size.gen0",
-		attribute:      "generation",
-		attributeValue: "gen0",
+		mappedName: "runtime.dotnet.gc.size.gen0",
+		attributes: []runtimeMetricAttribute{{
+			key:    "generation",
+			values: []string{"gen0"},
+		}},
 	}, {
-		mappedName:     "runtime.dotnet.gc.size.gen1",
-		attribute:      "generation",
-		attributeValue: "gen1",
+		mappedName: "runtime.dotnet.gc.size.gen1",
+		attributes: []runtimeMetricAttribute{{
+			key:    "generation",
+			values: []string{"gen1"},
+		}},
 	}, {
-		mappedName:     "runtime.dotnet.gc.size.gen2",
-		attribute:      "generation",
-		attributeValue: "gen2",
+		mappedName: "runtime.dotnet.gc.size.gen2",
+		attributes: []runtimeMetricAttribute{{
+			key:    "generation",
+			values: []string{"gen2"},
+		}},
 	}, {
-		mappedName:     "runtime.dotnet.gc.size.loh",
-		attribute:      "generation",
-		attributeValue: "loh",
+		mappedName: "runtime.dotnet.gc.size.loh",
+		attributes: []runtimeMetricAttribute{{
+			key:    "generation",
+			values: []string{"loh"},
+		}},
 	}},
 	"process.runtime.dotnet.gc.collections.count": {{
-		mappedName:     "runtime.dotnet.gc.count.gen0",
-		attribute:      "generation",
-		attributeValue: "gen0",
+		mappedName: "runtime.dotnet.gc.count.gen0",
+		attributes: []runtimeMetricAttribute{{
+			key:    "generation",
+			values: []string{"gen0"},
+		}},
 	}, {
-		mappedName:     "runtime.dotnet.gc.count.gen1",
-		attribute:      "generation",
-		attributeValue: "gen1",
+		mappedName: "runtime.dotnet.gc.count.gen1",
+		attributes: []runtimeMetricAttribute{{
+			key:    "generation",
+			values: []string{"gen1"},
+		}},
 	}, {
-		mappedName:     "runtime.dotnet.gc.count.gen2",
-		attribute:      "generation",
-		attributeValue: "gen2",
+		mappedName: "runtime.dotnet.gc.count.gen2",
+		attributes: []runtimeMetricAttribute{{
+			key:    "generation",
+			values: []string{"gen2"},
+		}},
 	}},
 }
 
@@ -95,40 +116,65 @@ var javaRuntimeMetricsMappings = map[string][]runtimeMetricMapping{
 	"process.runtime.jvm.threads.count": {{mappedName: "jvm.thread_count"}},
 	"process.runtime.jvm.gc.duration":   {{mappedName: "jvm.gc.parnew.time"}},
 	"process.runtime.jvm.memory.usage": {{
-		mappedName:     "jvm.heap_memory",
-		attribute:      "type",
-		attributeValue: "heap",
+		mappedName: "jvm.heap_memory",
+		attributes: []runtimeMetricAttribute{{
+			key:    "type",
+			values: []string{"heap"},
+		}},
 	}, {
-		mappedName:     "jvm.non_heap_memory",
-		attribute:      "type",
-		attributeValue: "non_heap",
+		mappedName: "jvm.non_heap_memory",
+		attributes: []runtimeMetricAttribute{{
+			key:    "type",
+			values: []string{"non_heap"},
+		}},
+	}, {
+		mappedName: "jvm.gc.old_gen_size",
+		attributes: []runtimeMetricAttribute{{
+			key:    "pool",
+			values: []string{"g1_old_gen", "ps_old_gen", "tenured_gen"},
+		}, {
+			key:    "type",
+			values: []string{"heap"},
+		}},
 	}},
 	"process.runtime.jvm.memory.committed": {{
-		mappedName:     "jvm.heap_memory_committed",
-		attribute:      "type",
-		attributeValue: "heap",
+		mappedName: "jvm.heap_memory_committed",
+		attributes: []runtimeMetricAttribute{{
+			key:    "type",
+			values: []string{"heap"},
+		}},
 	}, {
-		mappedName:     "jvm.non_heap_memory_committed",
-		attribute:      "type",
-		attributeValue: "non_heap",
+		mappedName: "jvm.non_heap_memory_committed",
+		attributes: []runtimeMetricAttribute{{
+			key:    "type",
+			values: []string{"non_heap"},
+		}},
 	}},
 	"process.runtime.jvm.memory.init": {{
-		mappedName:     "jvm.heap_memory_init",
-		attribute:      "type",
-		attributeValue: "heap",
+		mappedName: "jvm.heap_memory_init",
+		attributes: []runtimeMetricAttribute{{
+			key:    "type",
+			values: []string{"heap"},
+		}},
 	}, {
-		mappedName:     "jvm.non_heap_memory_init",
-		attribute:      "type",
-		attributeValue: "non_heap",
+		mappedName: "jvm.non_heap_memory_init",
+		attributes: []runtimeMetricAttribute{{
+			key:    "type",
+			values: []string{"non_heap"},
+		}},
 	}},
 	"process.runtime.jvm.memory.limit": {{
-		mappedName:     "jvm.heap_memory_max",
-		attribute:      "type",
-		attributeValue: "heap",
+		mappedName: "jvm.heap_memory_max",
+		attributes: []runtimeMetricAttribute{{
+			key:    "type",
+			values: []string{"heap"},
+		}},
 	}, {
-		mappedName:     "jvm.non_heap_memory_max",
-		attribute:      "type",
-		attributeValue: "non_heap",
+		mappedName: "jvm.non_heap_memory_max",
+		attributes: []runtimeMetricAttribute{{
+			key:    "type",
+			values: []string{"non_heap"},
+		}},
 	}},
 }
 
